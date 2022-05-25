166abe251d650f92f4740a9563dd8a1087defe32
Jordan Hall <jordan@libertyware.co.uk>
2022-05-25 15:13:21 +0100

    fix: move prop components to props folder ready for merging
    Signed-off-by: Jordan Hall <jordan@libertyware.co.uk>

--- tmp/uk_gov_dash_components/src/ts/components/props/AutoComplete.react.js
+++ tmp/uk_gov_dash_components/src/ts/components/props/AutoComplete.react.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { AutoComplete as RealComponent } from '../LazyLoader';
-import DropdownArrowDown from './dropdownArrowDown.react';
+import DropdownArrowDown from '../dropdownArrowDown.react';
 
 /**
  * Lazy loaded Autocomplete
