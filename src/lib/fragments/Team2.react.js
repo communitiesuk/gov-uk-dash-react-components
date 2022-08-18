60b49ebffb29e00fae401295a94fa6c6258eedaf
Kate Riley <kate.riley@communities.gov.uk>
2022-08-18 16:14:44 +0100

    add button counter component

--- src/lib/fragments/Team2.react.js
+++ src/lib/fragments/Team2.react.js
@@ -3,11 +3,24 @@ import React, { Component } from 'react';
 import { defaultProps, propTypes } from '../components/ComponentTemplate.react';
 
 class Team2 extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {clicks: 0 
+        }}
+    update_clicks = () => {
+        // update the state!
+        this.setState({
+            clicks: this.state.clicks+1
+        })
+        }
     render() {
         let date = new Date()
         return (
             <div id={this.props.id}>
                 {(date.toLocaleTimeString())}
+                <button onClick={() => this.update_clicks()}>
+                    You have clicked {this.state.clicks} times
+                </button>
             </div>
         )
     }
