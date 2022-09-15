373977354dad0b9d2ae601b709e848ad88be006b
Matt Pike <matt.pike@levellingup.gov.uk>
2022-09-15 16:07:17 +0000

    WIP: ready to experiment demo side nav options

--- src/lib/fragments/SideNavigation.react.js
+++ src/lib/fragments/SideNavigation.react.js
@@ -1,52 +1,18 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { SideNavigation as RealComponent } from '../LazyLoader';
+import React, { Component } from 'react';
 
-/**
- * Lazy loaded SideNavigation
- *
- * @param {
- * 	id,
- * } [props={}]
- * @return {*}
- */
-const SideNavigation = (props = {}) => {
-    return (
-        <RealComponent {...props} />
-    );
+import { defaultProps, propTypes } from '../components/SideNavigation.react';
+
+class SideNavigation extends Component {
+    render() {
+        return (
+            <div id={this.props.id}>
+                I love React.js!
+            </div>
+        )
+    }
 }
 
-/**
- * PropTypes is a part of React, see full documenation below.
- * https://reactjs.org/docs/typechecking-with-proptypes.html
- */
-
-SideNavigation.defaultProps = {
-    /**
-     * Any default prop values, e.g.
-     *   congratulatoryMessage: 'You are amazing'
-     */
-};
-
-SideNavigation.propTypes = {
-    /**
-     * Add any options you wish to pass to your component to this dictionary.
-     */
-
-    /**
-     * The ID of this component, used to identify dash components
-     * in callbacks. The ID needs to be unique across all of the
-     * components in an app.
-     */
-    id: PropTypes.string,
-
-    /**
-     * Dash-assigned callback that gets fired when the value changes.
-     */
-    setProps: PropTypes.func,
-};
-
-export const defaultProps = SideNavigation.defaultProps;
-export const propTypes = SideNavigation.propTypes;
+SideNavigation.defaultProps = defaultProps;
+SideNavigation.propTypes = propTypes;
 
 export default SideNavigation
