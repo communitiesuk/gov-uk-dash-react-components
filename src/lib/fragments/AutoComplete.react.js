f40c5b25a5f24cb7409cd93ab4cd8e607c791575
Ellie-Brakoniecki <112866399+Ellie-Brakoniecki@users.noreply.github.com>
2022-10-04 15:29:16 +0100

    Changing the way the focus is set in the dropdown so "no results found" works correctly

--- src/lib/fragments/AutoComplete.react.js
+++ src/lib/fragments/AutoComplete.react.js
@@ -455,7 +455,7 @@ const AutoComplete = (props) => {
 						setOptions(options);
 						setSelected((autoselect && optionsAvailable) ? 0 : -1);
 						setValidChoiceMade(false);
-						setFocus(selectElement ? (optionsAvailable ? 0 : -1) : focus)
+						setFocus(selectElement ? (optionsAvailable ? 0 : -1) : isFocus)
 					})
 				} else if (!newQuery.length || !queryLongEnough) {
 					setMenuOpen(false);
@@ -517,7 +517,7 @@ const AutoComplete = (props) => {
 	const showNoOptionsFoundRender = showNoOptionsFound &&
 		inputFocused && noOptionsAvailable && queryNotEmpty && queryLongEnough
 
-	const wrapperClassName = `${cssNamespace}__wrapper`
+		const wrapperClassName = `${cssNamespace}__wrapper`
 
 	const inputClassName = `${cssNamespace}__input`
 	const componentIsFocused = isFocus !== null
