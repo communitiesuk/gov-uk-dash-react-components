140031737639f0295a431e5777761bfbedd121e6
Kate Riley <kate.riley@communities.gov.uk>
2022-10-14 10:01:48 +0100

    WIP

--- src/lib/fragments/Accordion.react.js
+++ src/lib/fragments/Accordion.react.js
@@ -1,16 +1,100 @@
 import React, { Component } from 'react';
+import "../../dashboard.css"
 
 import { defaultProps, propTypes } from '../components/ComponentTemplate.react';
 
 class Accordion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hidden: props.collapsedByDefault
+    }
+  }
+
+  handleSubMenuVisibility = (event) => {
+      this.setState({
+        hidden: !this.state.hidden
+      })
+    
+
+  }
+
     render() {
+      const { expandedClass="govuk-accordion__section--expanded", collapsedClass="govuk-accordion__section"} = this.props
         return (
-            <div id={this.props.id}>
-                I love React.js!
-            </div>
+          <body class="govuk-template__body app-example-page js-enabled">
+          <div class="govuk-accordion" data-module="govuk-accordion" id="accordion-default">
+  <div class={this.state.hidden ? collapsedClass : collapsedClass +  expandedClass}>
+    <div class="govuk-accordion__section-header">
+      <h2 class="govuk-accordion__section-heading">
+        <button type="button" aria-controls="accordion-default-content-1" class="govuk-accordion__section-button" id="accordion-default-heading-1" aria-expanded={!this.state.hidden} onClick={this.handleSubMenuVisibility}>
+          <span class="govuk-accordion__section-heading-text" id="accordion-default-heading-1">
+            <span class="govuk-accordion__section-heading-text-focus"> Writing well for the web 
+            </span>
+          </span> 
+          <span class="govuk-visually-hidden govuk-accordion__section-heading-divider"></span>
+          <span class="govuk-accordion__section-toggle" data-nosnippet>
+            <span class="govuk-accordion__section-toggle-focus">
+              <span class="moj-side-navigation__item--collapsed">
+              </span>
+            </span>
+            <span class="govuk-accordion__section-toggle-text"> Show 
+              <span class="govuk-visually-hidden"> this section</span>
+            </span>
+          </span>
+        </button>
+      </h2>
+    </div>
+    <div id="accordion-default-content-1" class="govuk-accordion__section-content" aria-labelledby="accordion-default-heading-1">
+      <p class='govuk-body'>This is the content for Writing well for the web.</p>
+    </div>
+  </div>   
+  </div> 
+  </body>
         )
     }
-}
+  }
+
+  //   render() {
+  //     const { expandedClass="govuk-accordion__section--expanded", collapsedClass="govuk-accordion__section-content"} = this.props
+  //       return (
+  //         <div class="govuk-accordion" data-module="govuk-accordion" id="accordion-default">
+  //  <div class={this.state.hidden ? collapsedClass : expandedClass}>
+  //    <div class="govuk-accordion__section-header">
+  //      <h2 class="govuk-accordion__section-heading">
+  //        <span class="govuk-accordion__section-button" id="accordion-default-heading-1" aria-controls="expandable-menu-contents" aria-expanded={!this.state.hidden} onClick={this.handleSubMenuVisibility}>
+  //          Writing well for the web
+  //                   </span>
+  //      </h2>
+  //    </div>
+  //    <div id="accordion-default-content-1" class="govuk-accordion__section-content" aria-labelledby="accordion-default-heading-1">
+  //      <p class='govuk-body'>This is the content for Writing well for the web.</p>
+  //    </div>
+  //  </div>   
+  //  </div> 
+  //       )
+  //   }
+  // }
+
+
+
+
+
+
+
+
+
+
+
+    //         <div id={this.props.id} aria-controls="expandable-menu-contents" aria-expanded={!this.state.hidden} onClick={this.handleSubMenuVisibility}>
+    //             I love React.js!
+    //             <div className={this.state.hidden ? collapsedClass : expandedClass}>
+    //               Test!
+    //             </div>
+    //         </div>
+    //     )
+    // }
+// }
 
 Accordion.defaultProps = defaultProps;
 Accordion.propTypes = propTypes;
