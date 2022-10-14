53ef56cc5226255742216ae8317d388c83fb7aad
robertblincoe <robert.blincoe@levellingup.gov.uk>
2022-10-14 15:46:16 +0100

    wip

--- src/lib/fragments/Accordion.react.js
+++ src/lib/fragments/Accordion.react.js
@@ -19,16 +19,19 @@ class Accordion extends Component {
 
   }
 
-    render() {
-      const { expandedClass="govuk-accordion__section--expanded", collapsedClass="govuk-accordion__section",ShowString = "Show", HideString = "Hide", heading, children} = this.props
+  render() {
+    return (this.renderAccordionSection(1, this.props.heading, this.props.children, this.state.hidden))
+  }
+    renderAccordionSection(index, heading, children, hidden) {
+      const { expandedClass="govuk-accordion__section--expanded", collapsedClass="govuk-accordion__section",ShowString = "Show", HideString = "Hide"} = this.props
         return (
           <body className="govuk-template__body app-example-page js-enabled">
           {/* <div className="govuk-accordion" data-module="govuk-accordion" id="accordion-default"> */}
-  <div className={this.state.hidden ? collapsedClass : collapsedClass + ' ' + expandedClass}>
+  <div className={hidden ? collapsedClass : collapsedClass + ' ' + expandedClass}>
     <div className="govuk-accordion__section-header">
       <h2 className="govuk-accordion__section-heading">
-        <button type="button" aria-controls="accordion-default-content-1" className="govuk-accordion__section-button" id="accordion-default-heading-1" aria-expanded={!this.state.hidden} onClick={this.handleSubMenuVisibility}>
-          <span className="govuk-accordion__section-heading-text" id="accordion-default-heading-1">
+        <button type="button" aria-controls={`accordion-default-content-${index}`} className="govuk-accordion__section-button" id={`accordion-default-heading-${index}`} aria-expanded={!this.state.hidden} onClick={this.handleSubMenuVisibility}>
+          <span className="govuk-accordion__section-heading-text" id={`accordion-default-heading-${index}`}>
             <span className="govuk-accordion__section-heading-text-focus"> {heading} 
             </span>
           </span> 
@@ -38,15 +41,15 @@ class Accordion extends Component {
               <span className="moj-side-navigation__item--collapsed">
               </span>
             </span>
-            <span className= {this.state.hidden ? "govuk-accordion-nav__chevron govuk-accordion-nav__chevron--down" : "govuk-accordion-nav__chevron" } ></span>
-            <span className="govuk-accordion__section-toggle-text"> {this.state.hidden ? ShowString : HideString} 
+            <span className= {hidden ? "govuk-accordion-nav__chevron govuk-accordion-nav__chevron--down" : "govuk-accordion-nav__chevron" } ></span>
+            <span className="govuk-accordion__section-toggle-text"> {hidden ? ShowString : HideString} 
               <span className="govuk-visually-hidden"> this section</span>
             </span>
           </span>
         </button>
       </h2>
     </div>
-    <div id="accordion-default-content-1" className="govuk-accordion__section-content" aria-labelledby="accordion-default-heading-1">
+    <div id={`accordion-default-heading-${index}`} className="govuk-accordion__section-content" aria-labelledby={`accordion-default-heading-${index}`}>
       <p className='govuk-body'>{children}</p>
     </div>
   </div> 
