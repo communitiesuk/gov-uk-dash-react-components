36f5aae9aef460955ff3a99ad8d89d2fb9458894
balazsroman-tpx <115715271+balazsroman-tpx@users.noreply.github.com>
2023-05-17 15:01:45 +0100

    className instead of class

--- src/lib/fragments/Tabs.react.js
+++ src/lib/fragments/Tabs.react.js
@@ -8,29 +8,29 @@ class Tabs extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        tabSelected: this.props.defaultTab,
+      tabSelected: this.props.defaultTab,
     }
   }
 
-setSelectedTabIndex = (index) => {
+  setSelectedTabIndex = (index) => {
     this.setState({ tabSelected: index });
-  } 
+  }
 
-render() {
+  render() {
     const selectedTabContent = this.props.children[this.state.tabSelected]
-    let tabHeaders; 
-    if (this.props.children.length === 1 || xtype.type(this.props.children) !== 'array'){                     
+    let tabHeaders;
+    if (this.props.children.length === 1 || xtype.type(this.props.children) !== 'array') {
       tabHeaders = this.renderTabHeader(0, this.props.tabHeadings[0]);
     }
-    else { 
-      tabHeaders = this.props.tabHeadings.map((tabHeader,index) => this.renderTabHeader(index, tabHeader))
+    else {
+      tabHeaders = this.props.tabHeadings.map((tabHeader, index) => this.renderTabHeader(index, tabHeader))
     }
     return (
       <div className="js-enabled">
         <div className="govuk-tabs" data-module="govuk-tabs" id={this.props.id}>
-            <h2 class="govuk-tabs__title"> Contents </h2>
-            <ul class="govuk-tabs__list" role="tablist"> {tabHeaders} </ul>    
-            <div className="govuk-tabs__panel">{selectedTabContent} </div>        
+          <h2 className="govuk-tabs__title"> Contents </h2>
+          <ul className="govuk-tabs__list" role="tablist"> {tabHeaders} </ul>
+          <div className="govuk-tabs__panel">{selectedTabContent} </div>
         </div>
       </div>
     )
@@ -39,15 +39,15 @@ render() {
 
   renderTabHeader(index, tabHeader) {
     let tabIsSelected;
-    tabIsSelected = (index === this.state.tabSelected) ? true: false
-      return (   
-          <li className={tabIsSelected ? "tab-selected-underline govuk-tabs__list-item govuk-tabs__list-item--selected" : " tab-underline govuk-tabs__list-item"} role="presentation">
-            <button className={tabIsSelected ? "tab-button-selected govuk-tabs__tab":  "tab-button govuk-tabs__tab"} role = "tab" type="button" aria-controls={tabHeader}   aria-labelledby={tabHeader} aria-selected={tabIsSelected ? 'true':'false'} onClick={() => this.setSelectedTabIndex(index)}> {tabHeader} 
-            </button>
-          </li>            
-        )
-    }
+    tabIsSelected = (index === this.state.tabSelected) ? true : false
+    return (
+      <li className={tabIsSelected ? "tab-selected-underline govuk-tabs__list-item govuk-tabs__list-item--selected" : " tab-underline govuk-tabs__list-item"} role="presentation">
+        <button className={tabIsSelected ? "tab-button-selected govuk-tabs__tab" : "tab-button govuk-tabs__tab"} role="tab" type="button" aria-controls={tabHeader} aria-labelledby={tabHeader} aria-selected={tabIsSelected ? 'true' : 'false'} onClick={() => this.setSelectedTabIndex(index)}> {tabHeader}
+        </button>
+      </li>
+    )
   }
+}
 
 
 Tabs.defaultProps = defaultProps;
