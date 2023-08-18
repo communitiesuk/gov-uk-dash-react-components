ce943e6ce6efb25ba789d258c6e38965beaa8e4b
LukeDallimore <luke.dallimore@tpximpact.com>
2023-08-18 17:11:32 +0100

    Added cookies pages

---
+++ src/lib/cookies/CookiePages/Cookies.styles.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+import type { ComponentType } from 'react';
+
+
+export const Article: ComponentType<*> = (() =>
+    styled.article`
+        max-width: 50em;
+    `
+)();
+
+
+export const Heading = ({ className = '', ...props }) => (
+    <h2 className={ `govuk-heading-m ${className}` } { ...props }/>
+);
