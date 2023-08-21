339b16f26c7ded34d9e44d83bd8b0ef069ded437
urquha <andy.urquhart@hotmail.co.uk>
2023-08-21 12:07:16 +0100

    added link

--- src/lib/fragments/ChangeLogBanner.react.js
+++ src/lib/fragments/ChangeLogBanner.react.js
@@ -3,35 +3,32 @@ import moment from "moment";
 import React from 'react';
 import { defaultProps, propTypes } from '../components/ChangeLogBanner.react';
 import { ChangeLogBannerContainer, ChangeLogBannerSTY, ChangeLogBannerTag } from './ChangeLogBanner.styles';
-const ChangeLogBanner = (props) => {
-    const {
-        items,
-    } = { ...defaultProps, ...props };
-    // const item = {
-    //     type: "New",
-    //     date: "2020-10-01",
-    //     heading: "New data added to the site"
-    // };
 
-
+const ChangeLogBanner = ({ items = defaultProps.items, LinkComponent = 'a' }) => {
     return (
         <div className="changeLogBanner">
             <ChangeLogBannerContainer>
-                {items.map((item, index) => <ChangeLogBannerSTY>
-                    <div className={"govuk-body-s govuk-!-font-weight-bold govuk-!-margin-bottom-0"}>
-                        <ChangeLogBannerTag>
-                            {item.type}
-                        </ChangeLogBannerTag>
-                        <time dateTime={item.date}>
-                            {moment(item.date).format("D MMMM YYYY")}
-                        </time> &mdash; {item.heading}
-                        {/* <Link to={ `/details/whats-new/record/${ item.id }` }
-                      className={ "govuk-link govuk-link--no-visited-state govuk-!-margin-left-1" }>
-                    More
-                </Link> */}
-                    </div>
-                </ChangeLogBannerSTY>
-                )}
+                {items.map((item, index) => {
+                    {
+                        const linkProps = LinkComponent === 'a' ? { href: item.link } : { to: item.link }
+                        return (
+                            <ChangeLogBannerSTY key={index}>
+                                <div className={"govuk-body-s govuk-!-font-weight-bold govuk-!-margin-bottom-0"}>
+                                    <ChangeLogBannerTag>
+                                        {item.type}
+                                    </ChangeLogBannerTag>
+                                    <time dateTime={item.date}>
+                                        {moment(item.date).format("D MMMM YYYY")}
+                                    </time> &mdash; {item.heading}
+                                    <LinkComponent {...linkProps}
+                                        className={"govuk-link govuk-link--no-visited-state govuk-!-margin-left-1"}>
+                                        {item.linkTitle ? item.linkTitle : 'More'}
+                                    </LinkComponent>
+                                </div>
+                            </ChangeLogBannerSTY>
+                        )
+                    }
+                })}
             </ChangeLogBannerContainer>
         </div>
     );
