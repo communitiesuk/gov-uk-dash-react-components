8dc1a6efb7371714776d8a4979df05f61f216811
urquha <andy.urquhart@hotmail.co.uk>
2023-08-23 13:48:01 +0100

    fixed props

--- src/lib/components/ChangeLogBanner.react.js
+++ src/lib/components/ChangeLogBanner.react.js
@@ -2,11 +2,6 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { ChangeLogBanner as RealComponent } from '../LazyLoader';
 
-/**
- *Default dropdown arrow
- *
- * @param {string} { className }
- */
 const ChangeLogBanner = (props = {}) => {
     return (
         <RealComponent {...props} />
@@ -15,18 +10,32 @@ const ChangeLogBanner = (props = {}) => {
 
 ChangeLogBanner.propTypes = {
     /**
-     * Array of dictionaries representing items
+     * Array of dictionaries representing changelog updates
      */
-    items: PropTypes.arrayOf(
+    updates: PropTypes.arrayOf(
         PropTypes.shape({
-            key1: PropTypes.string,
-            key2: PropTypes.number,
-            key3: PropTypes.arrayOf(PropTypes.string),
+            type: PropTypes.string.isRequired,           // Type of change
+            date: PropTypes.string.isRequired,           // Date of the change
+            heading: PropTypes.string.isRequired,        // Heading or title of the change
+            link: PropTypes.string.isRequired,           // Link associated with the change
+            linkTitle: PropTypes.string,                 // Optional title of the link
         })
-    ),
+    ),  // Marked as required because the component uses it directly
+
+    /**
+     * Custom Link component or 'a'. 
+     * If it's a custom component, it should accept 'to' prop instead of 'href'.
+     */
+    LinkComponent: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.func,
+        PropTypes.object
+    ]),
 };
 
-export const defaultProps = ChangeLogBanner.defaultProps;
-export const propTypes = ChangeLogBanner.propTypes;
-export default ChangeLogBanner;
+ChangeLogBanner.defaultProps = {
+    updates: [],
+    LinkComponent: 'a'
+};
 
+export default ChangeLogBanner;
--- src/lib/fragments/ChangeLogBanner.react.js
+++ src/lib/fragments/ChangeLogBanner.react.js
@@ -4,25 +4,26 @@ import React from 'react';
 import { defaultProps, propTypes } from '../components/ChangeLogBanner.react';
 import { ChangeLogBannerContainer, ChangeLogBannerSTY, ChangeLogBannerTag } from './ChangeLogBanner.styles';
 
-const ChangeLogBanner = ({ items = defaultProps.items, LinkComponent = 'a' }) => {
+const ChangeLogBanner = ({ updates = defaultProps.updates, LinkComponent = 'a' }) => {
+    if (!updates.length) return null;
     return (
         <div className="changeLogBanner">
             <ChangeLogBannerContainer>
-                {items.map((item, index) => {
+                {updates.map((update, index) => {
                     {
-                        const linkProps = LinkComponent === 'a' ? { href: item.link } : { to: item.link }
+                        const linkProps = LinkComponent === 'a' ? { href: update.link } : { to: update.link }
                         return (
                             <ChangeLogBannerSTY key={index}>
                                 <div className={"govuk-body-s govuk-!-font-weight-bold govuk-!-margin-bottom-0"}>
                                     <ChangeLogBannerTag>
-                                        {item.type}
+                                        {update.type}
                                     </ChangeLogBannerTag>
-                                    <time dateTime={item.date}>
-                                        {moment(item.date).format("D MMMM YYYY")}
-                                    </time> &mdash; {item.heading}
+                                    <time dateTime={update.date}>
+                                        {moment(update.date).format("D MMMM YYYY")}
+                                    </time> &mdash; {update.heading}
                                     <LinkComponent {...linkProps}
                                         className={"govuk-link govuk-link--no-visited-state govuk-!-margin-left-1"}>
-                                        {item.linkTitle ? item.linkTitle : 'More'}
+                                        {update.linkTitle ? update.linkTitle : 'More'}
                                     </LinkComponent>
                                 </div>
                             </ChangeLogBannerSTY>
