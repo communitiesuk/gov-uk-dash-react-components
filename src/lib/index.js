1437ae4e04fbb9ee180480e4068dde690032cbfb
Andy Urquhart <35800749+urquha@users.noreply.github.com>
2023-09-08 13:58:48 +0100

    fixed order of component exports/imports (#99)
    * fixed order of component exports/imports
    
    * version

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,16 +1,22 @@
 /* eslint-disable import/prefer-default-export */
-import Accordion from './components/Accordion.react';
-import AdditionalDetails from './components/AdditionalDetails.react';
 import AutoComplete from './components/AutoComplete.react';
-import ChangeLogBanner from './components/ChangeLogBanner.react';
+import Dropdown from './components/Dropdown.react';
+import ComboBox from './components/ui/ComboBox.react';
 import CheckboxList from './components/CheckboxList.react';
 import ComponentTemplate from './components/ComponentTemplate.react';
-import Dropdown from './components/Dropdown.react';
 import ExpandableMenuItem from './components/ExpandableMenuItem.react';
+import Accordion from './components/Accordion.react';
 import Tabs from './components/Tabs.react';
-import ComboBox from './components/ui/ComboBox.react';
+import ChangeLogBanner from './components/ChangeLogBanner.react';
 
 export {
-    Accordion, AdditionalDetails, AutoComplete, ChangeLogBanner, CheckboxList, ComboBox, ComponentTemplate, Dropdown, ExpandableMenuItem, Tabs
+    AutoComplete,
+    Dropdown,
+    ComboBox,
+    CheckboxList,
+    ComponentTemplate,
+    ExpandableMenuItem,
+    Accordion,
+    Tabs,
+    ChangeLogBanner
 };
-
