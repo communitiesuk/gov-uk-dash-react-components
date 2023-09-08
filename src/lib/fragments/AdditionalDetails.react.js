f054be5e362fa3128c0cef1b2c48291877ffca3f
urquha <andy.urquhart@hotmail.co.uk>
2023-09-08 16:27:49 +0100

    fix

--- src/lib/fragments/AdditionalDetails.react.js
+++ src/lib/fragments/AdditionalDetails.react.js
@@ -1,13 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const AdditionalDetails = ({ id, summaryText, detailsText, hidden }) => {
     if (hidden) {
-        return null
-    } 
+        return null;
+    }
+
     const [isOpen, setIsOpen] = useState(false);
+    const detailsRef = useRef(null);
+
+    const handleToggle = () => {
+        if (detailsRef.current) {
+            setIsOpen(detailsRef.current.open);
+        }
+    };
+
+    useEffect(() => {
+        // Initially sync state with ref's open attribute
+        if (detailsRef.current) {
+            setIsOpen(detailsRef.current.open);
+        }
+    }, []);
 
     return (
-        <details id={id} className="govuk-details" data-module="govuk-details" open={isOpen} onClick={() => setIsOpen(!isOpen)}>
+        <details ref={detailsRef} id={id} className="govuk-details" data-module="govuk-details" open={isOpen} onClick={handleToggle}>
             <summary className="govuk-details__summary">
                 <span className="govuk-details__summary-text">
                     {summaryText}
