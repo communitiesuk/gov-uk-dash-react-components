f5a05c324a733d682cea6006fe2c3db7e0b19189
Kate Riley <kate.riley@communities.gov.uk>
2024-06-20 11:26:32 +0100

    update Radios component

--- src/lib/components/Radios.react.js
+++ src/lib/components/Radios.react.js
@@ -7,6 +7,8 @@ import { Radios as RealComponent } from '../LazyLoader';
  *
  * @param {
  * 	id,
+ *  options,
+ *  value,
  * } [props={}]
  * @return {*}
  */
@@ -22,16 +24,79 @@ const Radios = (props = {}) => {
  */
 
 Radios.defaultProps = {
-    /**
-     * Any default prop values, e.g.
-     *   congratulatoryMessage: 'You are amazing'
-     */
+    options: [],
+    value: [],
 };
 
 Radios.propTypes = {
     /**
-     * Add any options you wish to pass to your component to this dictionary.
+     * An array of options
      */
+    options: PropTypes.oneOfType([
+        /**
+         * Array of options where the label and the value are the same thing - [string|number|bool]
+         */
+        PropTypes.arrayOf(
+            PropTypes.oneOfType([
+                PropTypes.string,
+                PropTypes.number,
+                PropTypes.bool,
+            ])
+        ),
+        /**
+         * Simpler `options` representation in dictionary format. The order is not guaranteed.
+         * {`value1`: `label1`, `value2`: `label2`, ... }
+         * which is equal to
+         * [{label: `label1`, value: `value1`}, {label: `label2`, value: `value2`}, ...]
+         */
+        PropTypes.object,
+        /**
+         * An array of options {label: [string|number], value: [string|number]},
+         * an optional disabled field can be used for each option
+         */
+        PropTypes.arrayOf(
+            PropTypes.exact({
+                /**
+                 * The option's label
+                 */
+                label: PropTypes.node.isRequired,
+
+                /**
+                 * The value of the option. This value
+                 * corresponds to the items specified in the
+                 * `value` property.
+                 */
+                value: PropTypes.oneOfType([
+                    PropTypes.string,
+                    PropTypes.number,
+                    PropTypes.bool,
+                ]).isRequired,
+
+                /**
+                 * If true, this option is disabled and cannot be selected.
+                 */
+                disabled: PropTypes.bool,
+
+                /**
+                 * The HTML 'title' attribute for the option. Allows for
+                 * information on hover. For more information on this attribute,
+                 * see https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/title
+                 */
+                title: PropTypes.string,
+            })
+        ),
+    ]),
+
+    /**
+     * The currently selected value
+     */
+    value: PropTypes.arrayOf(
+        PropTypes.oneOfType([
+            PropTypes.string,
+            PropTypes.number,
+            PropTypes.bool,
+        ])
+    ),   
 
     /**
      * The ID of this component, used to identify dash components
